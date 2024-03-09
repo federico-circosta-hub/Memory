5cb4378a85d47f35de56b441a44fb706ff2936e5
fridericus-hub <fed.circosta@gmail.com>
2024-03-09 18:19:53 +0100

    changed question card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -3,7 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import questionMark from "../img/cool-question-mark-sticker.webp";
+import questionMark from "../img/questionCard.jpeg";
 
 export const GameCard = ({
   animal,
