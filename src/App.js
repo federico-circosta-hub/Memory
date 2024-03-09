c764c63c0247c43ca0239f006a1e13025551a0c0
fridericus-hub <fed.circosta@gmail.com>
2024-03-09 18:37:19 +0100

    import bg bug fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [r] = useState(rows(difficulty));
   const [c] = useState(columns(difficulty));
   return (
-    <div className='p-8 flex flex-col justify-center align-center bg-[url("./img/questioning-concept-with-question-mark_23-2150408209.avif")] bg-no-repeat bg-cover bg-center'>
+    <div className='p-8 flex flex-col justify-center align-center bg-[url("./media/miscImages/questioning-concept-with-question-mark_23-2150408209.avif")] bg-no-repeat bg-cover bg-center'>
       <ChooseDifficulty setDifficulty={setDifficulty} />
       <Game rows={r} col={c} />
     </div>
